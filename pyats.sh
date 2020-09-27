0c9cba1b8edf68997dd26e529962ad8569f59081
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2020-09-27 12:39:31 +0000

    Fix: fix pyats-command

--- pyats.sh
+++ pyats.sh
@@ -2,4 +2,5 @@ date
 pipenv shell
 time pyats run genie --trigger-datafile ~/pyats/${1}/blitz.yaml --trigger-groups "And('enabled')" --testbed-file ~/pyats/${1}/testbed.yaml --subsection-datafile ~/pyats/subsection_datafile.yaml
 fuser -k 60001/tcp
-nohup pyats logs view --host 0.0.0.0 --port 60001 & > /dev/null
+pyats logs view --host 0.0.0.0 --port 60001 > /dev/null
+#nohup pyats logs view --host 0.0.0.0 --port 60001 > /dev/null &
